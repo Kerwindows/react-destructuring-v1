8e3de2cb5cb45f42ea1870925a4a79a09f4c7363
kerwindows <kerwindows@hotmail.com>
2022-09-30 10:31:32 -0400

    Updates

---
+++ precache-manifest.3c7b0d5aa08ab4e9afd3bef2d7940093.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd054f54689daf49ece5d8f9b25e95e",
+    "url": "/react-destructuring-v1/index.html"
+  },
+  {
+    "revision": "06fff1055c0c46f0f24e",
+    "url": "/react-destructuring-v1/static/js/2.d8630df9.chunk.js"
+  },
+  {
+    "revision": "595aa0d5212238805d8e",
+    "url": "/react-destructuring-v1/static/js/main.1934011d.chunk.js"
+  },
+  {
+    "revision": "007d36e99bb5308c6247",
+    "url": "/react-destructuring-v1/static/js/runtime-main.f53760ed.js"
+  }
+]);
